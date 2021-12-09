45cbff73924d43a094e74733faabe90b8b29276c
iam2804320 <ceremis1hagd@gmail.com>
2021-12-09 20:10:42 +0100

    v2

--- src/main/java/com/example/animeAPP/domain/model/Anime.java
+++ src/main/java/com/example/animeAPP/domain/model/Anime.java
@@ -1,6 +1,9 @@
 package com.example.animeAPP.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -8,10 +11,23 @@ import java.util.UUID;
 public class Anime {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    public UUID animeID;
+    public UUID animeid;
     public String name;
     public String description;
     public String type;
     public int year_release;
     public String imageurl;
+
+    @ManyToMany
+    @JoinTable(name="anime_author", joinColumns = @JoinColumn(name = "animeid"), inverseJoinColumns = @JoinColumn(name = "authorid"))
+    @JsonIgnoreProperties("animes")
+    public Set<Author> authors;
+
+    @ManyToMany
+    @JoinTable(name="anime_genre", joinColumns = @JoinColumn(name = "animeid"), inverseJoinColumns = @JoinColumn(name = "genreid"))
+    @JsonIgnoreProperties("animes")
+    public Set<Genre> genres;
+
+
+
 }
